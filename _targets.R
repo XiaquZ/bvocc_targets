9e562f8084e36d684847e8cc7d0651dedff7e794
Xiaqu Zhou <u0142858@kuleuven.be>
2025-02-28 18:07:17 +0100

    Update maximum distance.

--- _targets.R
+++ _targets.R
@@ -33,9 +33,9 @@ tar_source()
 
 tar_plan(
   tolerance = 0.25,
-  max_distance = 75000,
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/BVoMC/preIT/", full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/BVoMC/futIT/", full.names = T),
+  max_distance = 100000,
+  present_files = list.files("/lustre1/scratch/348/vsc34871/input/BVoMC/preFrance/", full.names = T),
+  future_files = list.files("/lustre1/scratch/348/vsc34871/input/BVoMC/futFrance/", full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
       gsub("ForestMAT_", "", tail(str_split(future_files, "/")[[1]], 1)),
