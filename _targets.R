f823593de55c24b4fa41c344ff8ffb1023bcd44a
XiaquZ <xiaqu.zhou@gmail.com>
2025-05-10 11:42:02 +0200

    BVoMC for eastEU.

--- _targets.R
+++ _targets.R
@@ -34,9 +34,11 @@ tar_source()
 tar_plan(
   tolerance = 0.25,
   max_distance = 75000, #!!!Always check this value!!!
-  present_files = list.files("/lustre1/scratch/348/vsc34871/input/BVoMC/preIT/",
+  present_files = list.files(
+    "/lustre1/scratch/348/vsc34871/input/BVoMC/preEastEU/",
    full.names = T),
-  future_files = list.files("/lustre1/scratch/348/vsc34871/input/BVoMC/futIT/",
+  future_files = list.files(
+    "/lustre1/scratch/348/vsc34871/input/BVoMC/futEastEU/",
    full.names = T),
   tar_target(tile_names,
     paste0(paste0(str_split(
