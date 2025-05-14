3f58bec0862aeeeba6f0e7843cead88beee65466
XiaquZ <xiaqu.zhou@gmail.com>
2025-05-14 10:34:27 +0200

    Northeast EU bvomc 75km SR.

--- R/calc_backward_vel.R
+++ R/calc_backward_vel.R
@@ -42,7 +42,7 @@ calc_backward_vel <- function(tile_name,
   names(distance) <- "distance"
   # Save results as rasters.
   backward_vel_file <- paste0(
-    "/lustre1/scratch/348/vsc34871/output/BVoMC/NorthEU/bvomc_75kmSR_",
+    "/lustre1/scratch/348/vsc34871/output/BVoMC/NortheastEU/bvomc_75kmSR_",
     tile_name, ".tif"
   )
   backward_vel <- mask(distance, distance <= max_distance, maskvalues = F) / 75 # Calculate velocity
